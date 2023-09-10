d1601d300984e8649c22b2cb0c593a7076f76ae7
shayan heidary <hdrydeveloper@gmail.com>
2023-09-10 16:06:34 +0200

    added feature for clearnigsearch with eventlistener

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,30 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Search = ({ query, setQuery }) => {
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    const callBack = (e) => {
+      if (document.activeElement === searchRef.current) {
+        console.log(document.activeElement, searchRef.current);
+        return;
+      }
+
+      if (e.code === "Enter") {
+        searchRef.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callBack);
+
+    return () => {
+      document.addEventListener("keydown", callBack);
+    };
+  }, []);
   return (
     <input
+      ref={searchRef}
       className="search"
       type="text"
       placeholder="Search movies..."
